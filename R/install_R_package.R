d8d1df6e4d2ad7871e60057e08d9ea090be2d51d
Christopher D. Long <galizur@gmail.com>
2019-09-18 01:27:52 -0400

    Now also checks R-Forge; http -> https.

--- R/install_R_package.R
+++ R/install_R_package.R
@@ -4,15 +4,16 @@ package <- commandArgs(TRUE)[1]
 
 lcpackage <- tolower(package)
 
-r["CRAN"] <- "http://ftp.osuosl.org/pub/cran"
+r["CRAN"] <- "https://ftp.osuosl.org/pub/cran"
+r["R-Forge"] <- "https://r-forge.r-project.org"
 options(repos=r)
 
 if (lcpackage %in% tolower(rownames(available.packages()))) {
-   print(paste("Package ",package," found in CRAN.",sep=""))
+   print(paste("Package ",package," found in CRAN/R-Forge.",sep=""))
    print(paste("Installing ",package,".",sep=""))
    install.packages(package,dependencies=TRUE)
 } else {
-  print(paste("Package ",package," not found in CRAN.",sep=""))
+  print(paste("Package ",package," not found in CRAN/R-Forge.",sep=""))
   print("Checking Bioconductor.")
   r <- BiocInstaller::biocinstallRepos()
   options(repos=r)
@@ -21,10 +22,8 @@ if (lcpackage %in% tolower(rownames(available.packages()))) {
     print(paste("Installing ",package,".",sep=""))
     install.packages(package,dependencies=TRUE)
     } else {
-    print(paste("Package ",package," not found in Bioconductor.",sep=""))
+        print(paste("Package ",package," not found in Bioconductor.",sep=""))
   }
 }
 
 q("no")
-
-#set repos then check, e.g., pkg %in% rownames(available.packages(repos=BiocInstaller::biocinstallRepos())) (tolower() ?)
